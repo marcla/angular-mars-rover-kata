f6139c0457eb2560d77dcf152a2efc6fa9537142
Claudio Marchiori <claudio.marchiori@codiceplastico.com>
2024-02-12 16:43:39 +0100

    feat(app): 🎸 create a new project for mars rover

---
+++ projects/mars-rover/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mr-root',
+  standalone: true,
+  imports: [],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'mars-rover';
+}
